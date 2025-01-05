4eb65cc886cdf282924dc063a1022b0c232ce712
DruChill <73948926@elp.edu.pe>
2025-01-04 21:57:00 -0500

    Agregar soporte para dotenv-java y configuración de datasource desde variables de entorno

--- src/main/java/com/viaRapida/viaRapidaProyect/ViaRapidaProyectApplication.java
+++ src/main/java/com/viaRapida/viaRapidaProyect/ViaRapidaProyectApplication.java
@@ -3,10 +3,21 @@ package com.viaRapida.viaRapidaProyect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class ViaRapidaProyectApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+
+
 		SpringApplication.run(ViaRapidaProyectApplication.class, args);
 	}
 
