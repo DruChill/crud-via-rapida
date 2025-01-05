bfda379d0c19bf0f859be3fda674b9e238fd38c6
DruChill <73948926@elp.edu.pe>
2025-01-05 16:04:25 -0500

    Agregar funcionalidad para mostrar asientos comprados y mejorar estilos de interfaz

--- src/main/java/com/viaRapida/viaRapidaProyect/controller/DestinationController.java
+++ src/main/java/com/viaRapida/viaRapidaProyect/controller/DestinationController.java
@@ -1,23 +1,39 @@
 package com.viaRapida.viaRapidaProyect.controller;
 
 import com.viaRapida.viaRapidaProyect.model.Destination;
+import com.viaRapida.viaRapidaProyect.model.Ticket;
 import com.viaRapida.viaRapidaProyect.service.DestinationService;
+import com.viaRapida.viaRapidaProyect.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class DestinationController {
     @Autowired
     private DestinationService destinationService;
 
+    @Autowired
+    private TicketService ticketService;
+
     @GetMapping("/")
     public String viewHomePage(Model model) {
         List<Destination> listDestinations = destinationService.getAllDestinations();
         model.addAttribute("listDestinations", listDestinations);
+
+        List<String> purchasedSeats = ticketService.getAllTickets().stream()
+                .map(Ticket::getSeatNumber)
+                .collect(Collectors.toList());
+        model.addAttribute("purchasedSeats", purchasedSeats);
+
+        List<String> seatList = Arrays.asList("1-A", "2-A", "3-A", "4-A");
+        model.addAttribute("seatList", seatList);
+
         return "index";
     }
 }
--- src/main/java/com/viaRapida/viaRapidaProyect/service/TicketService.java
+++ src/main/java/com/viaRapida/viaRapidaProyect/service/TicketService.java
@@ -2,6 +2,9 @@ package com.viaRapida.viaRapidaProyect.service;
 
 import com.viaRapida.viaRapidaProyect.model.Ticket;
 import com.viaRapida.viaRapidaProyect.repository.TicketRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +15,9 @@ public class TicketService {
 
     public void saveTicket(Ticket ticket) {
         ticketRepository.save(ticket);
+    }
+
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
     }
 }
