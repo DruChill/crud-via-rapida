a72f339c3b096022b467308a70db2ce5eb431b52
MarianPecho <mariana.bautists2004@gmail.com>
2025-01-16 22:40:11 -0500

    modif aplication

--- src/main/java/com/viaRapida/viaRapidaProyect/ViaRapidaProyectApplication.java
+++ src/main/java/com/viaRapida/viaRapidaProyect/ViaRapidaProyectApplication.java
@@ -3,7 +3,6 @@ package com.viaRapida.viaRapidaProyect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
@@ -11,11 +10,7 @@ public class ViaRapidaProyectApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
+		
 
 
 		SpringApplication.run(ViaRapidaProyectApplication.class, args);
